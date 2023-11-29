b7ba275f9110801628d557f97d6e8e0e9908ddc4
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 15:22:07 +0100

    demo

--- hello-world.sh
+++ hello-world.sh
@@ -1,26 +1,2 @@
-{
-  description = "Un simple projet Hello World";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";  // Choisissez la version de Nixpkgs que vous souhaitez
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      mkPackage = pkgs: pkgs.callPackage ./default.nix {};
-    in
-    (flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        packages.hello-world = mkPackage pkgs;
-        defaultPackage = self.packages."${system}".hello-world;
-        apps.hello-world = flake-utils.lib.mkApp {
-          drv = self.packages."${system}".hello-world;
-        };
-        apps.default = self.apps."${system}".hello-world;
-      })
-    ) // {
-      overlays.default = final: prev: {
-        hello-world = mkPackage final;
-      };
-    };
-}
+#!/usr/bin/env bash
+echo "Hello World"
