bb7ededbcc8abbac592a77c311939fc26e31091e
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 17:26:42 +0100

    demo

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     packages.x86_64-linux.hello-world =
       let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        pkgs = nixpkgs.legacyPackages.aarch64-linux;
       in
       pkgs.writeShellScriptBin "hello-world" ''
         DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
