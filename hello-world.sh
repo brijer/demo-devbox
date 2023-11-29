37bcfff0da705ec831553e4f8f990a3cebf847da
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 14:52:24 +0100

    demo

--- hello-world.sh
+++ hello-world.sh
@@ -1,2 +1,20 @@
-#!/usr/bin/env bash
-echo "Hello World"
+{
+  description = "Un simple projet Hello World";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.05";  // Remplacez par la version de NixOS que vous souhaitez utiliser
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.hello-world = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+      name = "hello-world";
+      src = ./hello-world.sh;
+      phases = [ "installPhase" ];
+      installPhase = ''
+        mkdir -p $out/bin
+        cp $src $out/bin/hello-world
+        chmod +x $out/bin/hello-world
+      '';
+    };
+  };
+}
