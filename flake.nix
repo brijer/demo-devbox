0ee983b3b521ce828512d9e9bbdf499c20afe2c0
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 17:56:48 +0100

    demo

--- flake.nix
+++ flake.nix
@@ -2,11 +2,23 @@
   description = "A simple script";
 
   outputs = { self, nixpkgs }: {
+    # Définition du package pour aarch64-linux
     defaultPackage.aarch64-linux = self.packages.aarch64-linux.hello-world;
 
+    # Définition du package pour x86_64-linux
     packages.x86_64-linux.hello-world =
       let
-        pkgs = nixpkgs.legacyPackages.aarch64-linux;
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+      pkgs.writeShellScriptBin "hello-world" ''
+        DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
+        ${pkgs.cowsay}/bin/cowsay Hello, world! Today is $DATE.
+      '';
+
+    # Ajouter ici la définition pour aarch64-darwin
+    packages.aarch64-darwin.hello-world =
+      let
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
       in
       pkgs.writeShellScriptBin "hello-world" ''
         DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
