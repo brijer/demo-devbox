7ea77844d023d591e90329b2559478baa5aaf38b
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 15:13:34 +0100

    demo

--- flake.nix
+++ flake.nix
@@ -1,17 +1,11 @@
 {
-  description = "Un simple projet Hello World";
+  description = "A very basic flake";
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello-world = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
-      name = "hello-world";
-      src = ./hello-world.sh;  // Assurez-vous que ce script est dans le même dossier que flake.nix
-      installPhase = ''
-        mkdir -p $out/bin
-        cp $src $out/bin/hello-world
-        chmod +x $out/bin/hello-world
-      '';
-    };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello-world;
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
   };
 }
