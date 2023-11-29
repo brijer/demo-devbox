70bdf2ef6226edf5f8555ff45d37103eabafba23
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 14:58:39 +0100

    demo

--- hello-world.sh
+++ hello-world.sh
@@ -1,15 +1,12 @@
 {
   description = "Un simple projet Hello World";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.05";  // Remplacez par la version de NixOS que vous souhaitez utiliser
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  // ou une autre branche ou tag de votre choix
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello-world = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+    defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
       name = "hello-world";
       src = ./hello-world.sh;
-      phases = [ "installPhase" ];
       installPhase = ''
         mkdir -p $out/bin
         cp $src $out/bin/hello-world
