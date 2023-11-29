ad7876208ce92b60e60953d46cb6cfe31eac9f8c
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-29 15:00:35 +0100

    demo

--- hello-world.sh
+++ hello-world.sh
@@ -1,17 +1,26 @@
 {
   description = "Un simple projet Hello World";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  // ou une autre branche ou tag de votre choix
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";  // Choisissez la version de Nixpkgs que vous souhaitez
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
-      name = "hello-world";
-      src = ./hello-world.sh;
-      installPhase = ''
-        mkdir -p $out/bin
-        cp $src $out/bin/hello-world
-        chmod +x $out/bin/hello-world
-      '';
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      mkPackage = pkgs: pkgs.callPackage ./default.nix {};
+    in
+    (flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        packages.hello-world = mkPackage pkgs;
+        defaultPackage = self.packages."${system}".hello-world;
+        apps.hello-world = flake-utils.lib.mkApp {
+          drv = self.packages."${system}".hello-world;
+        };
+        apps.default = self.apps."${system}".hello-world;
+      })
+    ) // {
+      overlays.default = final: prev: {
+        hello-world = mkPackage final;
+      };
     };
-  };
 }
