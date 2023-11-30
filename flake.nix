f05824a689eda2d9a56fed4699a6ce520c4bc45b
Jeremy BRISSON <j.brisson@betclicgroup.com>
2023-11-30 07:01:41 +0100

    demo

--- flake.nix
+++ flake.nix
@@ -1,25 +1,14 @@
 {
-  description = "A best script!";
+  description = "my project description";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        my-name = "hello-world";
-        my-buildInputs = with pkgs; [ cowsay ddate ];
-        hello-world = (pkgs.writeScriptBin my-name (builtins.readFile ./hello-world.sh)).overrideAttrs(old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
-      in rec {
-        defaultPackage = packages.hello-world;
-        packages.hello-world = pkgs.symlinkJoin {
-          name = my-name;
-          paths = [ hello-world ] ++ my-buildInputs;
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
-        };
-      }
-    );
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
 }
